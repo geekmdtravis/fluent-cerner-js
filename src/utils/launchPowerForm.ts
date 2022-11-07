c972e034f367fb6b7c8de112a4783714348f4a95
dl2github <daniel.lara2@cdcr.ca.gov>
2022-11-07 14:50:05 -0800

    comments added to launchPowerForm.ts

--- src/utils/launchPowerForm.ts
+++ src/utils/launchPowerForm.ts
@@ -1,3 +1,11 @@
+/**
+ * A type which represents the parameters to be be passed into the launchPowerForm() function.
+ * @param {number} personId - The person_id of the patient whose clinical note is to be displayed.
+ * @param {number} encounterId - The encntr_id of the patient whose clinical note is to be displayed.
+ * @param {string} target - Determines whether to target the "form", "activity", or the "ad hoc" charting dialog box.
+ * @param {number} targetId - The id number of the target document -- required only for `form` or `activity` targets.
+ * @param {string} permissions - The permissions to open the document with. Choices are "modify" or "read-only".
+ **/
 export type PowerFormOpts = {
   personId: number;
   encounterId: number;
@@ -6,11 +14,20 @@ export type PowerFormOpts = {
   permissions: 'modify' | 'read only';
 };
 
+/**
+ * A type which represents the object to be returned from the launchClinicalNote() function.
+ * @param {string} eventString - The string version of the MPageEvent
+ * @param {boolean} inPowerChart - Returns `true` if being run from inside of PowerChart and returns `false` otherwise.
+ **/
 export type MPageEventReturn = {
   eventString: string;
   inPowerChart: boolean;
 };
 
+/**
+ * A function to launch a power form, which returns an object of `MPageEventReturn`
+ * @param {PowerFormOpts} opts - The parameters passed, as specified in `PowerFormOpts`
+ **/
 export const launchPowerForm = (opts: PowerFormOpts): MPageEventReturn => {
   const { personId, encounterId, target, targetId, permissions } = opts;
   const params: Array<string> = [`${personId}`, `${encounterId}`];
@@ -54,6 +71,5 @@ export const launchPowerForm = (opts: PowerFormOpts): MPageEventReturn => {
   }
 
   returnObject.eventString = pfSentence;
-
   return returnObject;
 };
