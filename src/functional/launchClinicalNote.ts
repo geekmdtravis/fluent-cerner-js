807334cc8896921f7b87cc33253cc7949577d026
geekmdtravis <travis@geekmd.io>
2022-11-08 21:56:05 -0800

    performed first review of launchClinicalNote

--- src/functional/launchClinicalNote.ts
+++ src/functional/launchClinicalNote.ts
@@ -1,12 +1,24 @@
 import { MPageEventReturn } from '.';
+import { outsideOfPowerChartError } from '../utils';
 
 /**
+ * A type alias with the purpose of limiting the set of view flags available.
+ */
+export type ViewFlag =
+  | 'menu'
+  | 'buttons'
+  | 'toolbar'
+  | 'calculator'
+  | 'view-only';
+
+// TODO: Determine which of these fields are required and which are optional. Understand what each is.
+/**
  * A type which represents the parameters to be be passed into the launchClinicalNote() function
- * @param {number} personId - The person_id of the patient whose clinical note is to be displayed.
- * @param {number} encounterId - The encntr_id of the patient whose clinical note is to be displayed.
+ * @param {number} personId - The personId of the patient to whom the note is regarding.
+ * @param {number} encounterId - The encounterId of the encounter for the patient to which the note is regarding.
  * @param {Array<number>} eventIds - An array of event_ids of the clinical note(s) to be displayed.
  * @param {string} windowTitle - The text to be displayed in the first section of the title for the Clinical Notes window.
- * @param {Array<string>} viewOptionFlags - A set of flags that can be used to define the style of the Clinical Notes window. Example: ["menu", "buttons", "toolbar", calculator", "view-only"]
+ * @param {Array<ViewFlag>} viewOptionFlags - A set of flags that can be used to define the style of the Clinical Notes window. Example: ["menu", "buttons", "toolbar", calculator", "view-only"]
  * @param {string} viewName - The view name for the view-level preference of the tab to model the preferences after.
  * @param {number} viewSeq - The view sequence for the view-level preference of the tab to model the preferences after. An invalid viewSeq loads the clinical note with the default preferences.
  * @param {string} compName - The component name for the component-level preference of the tab to model the preferences after. An invalid compName loads the clinical note with the default preferences.
@@ -17,7 +29,7 @@ export type ClinicalNoteOpts = {
   encounterId: number;
   eventIds: Array<number>;
   windowTitle: string;
-  viewOptionFlags: Array<string>;
+  viewOptionFlags: Array<ViewFlag>;
   viewName: string;
   viewSeq: number;
   compName: string;
@@ -42,58 +54,40 @@ export const launchClinicalNote = (
     compName,
     compSeq,
   } = opts;
+  let inPowerChart = true;
   const params: Array<string> = [`${personId}`, `${encounterId}`];
-  const formattedEventIds = `${eventIds.join('|')}`;
 
-  params.push(`[${formattedEventIds}]`);
+  params.push(`[${eventIds.join('|')}]`);
   params.push(`${windowTitle}`);
-
-  let viewOptionFlagTotal = 0;
-  const uniqueFlags = Array.from(new Set(viewOptionFlags));
-  uniqueFlags.forEach(flag => {
-    if (flag === 'menu') {
-      viewOptionFlagTotal += 1;
-    }
-    if (flag === 'buttons') {
-      viewOptionFlagTotal += 2;
-    }
-    if (flag === 'toolbar') {
-      viewOptionFlagTotal += 4;
-    }
-    if (flag === 'calculator') {
-      viewOptionFlagTotal += 8;
-    }
-    if (flag === 'view-only') {
-      viewOptionFlagTotal += 16;
-    }
-  });
-  params.push(`${viewOptionFlagTotal}`);
-
+  params.push(`${calculateViewOptionFlag(viewOptionFlags)}`);
   params.push(`${viewName}`);
   params.push(`${viewSeq}`);
   params.push(`${compName}`);
   params.push(`${compSeq}`);
 
-  const returnObject: MPageEventReturn = {
-    eventString: '',
-    inPowerChart: true,
-  };
-
-  const pfSentence = `${params.join('|')}`;
-
+  const eventString = `${params.join('|')}`;
   try {
-    window.MPAGES_EVENT('POWERFORM', pfSentence);
+    window.MPAGES_EVENT('CLINICALNOTE', eventString);
   } catch (e) {
-    if (
-      e instanceof TypeError &&
-      e.message === 'window.MPAGES_EVENT is not a function'
-    ) {
-      returnObject.inPowerChart = false;
+    if (outsideOfPowerChartError(e)) {
+      inPowerChart = false;
+      console.warn(`window.MPAGES_EVENT('CLINICALNOTE', ${eventString})`);
     } else {
       throw e;
     }
   }
 
-  returnObject.eventString = pfSentence;
-  return returnObject;
+  return { inPowerChart, eventString };
 };
+
+function calculateViewOptionFlag(viewOptionFlags: Array<ViewFlag>): number {
+  let total = 0;
+  viewOptionFlags.forEach(flag => {
+    if (flag === 'menu') total += 1;
+    if (flag === 'buttons') total += 2;
+    if (flag === 'toolbar') total += 4;
+    if (flag === 'calculator') total += 8;
+    if (flag === 'view-only') total += 16;
+  });
+  return total;
+}
