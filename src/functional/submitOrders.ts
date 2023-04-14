f85205c176c7e1958a4b541df0f04e717afcc30c
geekmdtravis <travis@geekmd.io>
2023-04-14 13:51:14 -0700

    implemented tasks #34 #40 #41 and #42 to disable the need for specifying whether or not to disable power plans via na option

--- src/functional/submitOrders.ts
+++ src/functional/submitOrders.ts
@@ -14,35 +14,37 @@ const tabsMap = new Map<string, { tab: number; display: number }>()
 
 /**
  * @action `targetTab` - (optional) Sets the tab to be displayed, with and without power orders.
- * If not provided, will default to `power orders`, that is the orders tab with power orders enabled.
+ * If not provided, will default to `orders`, that is the orders tab with _PowerOrders_ disabled.
+ * Any tab with the term _power_ in it will enable both _PowerOrders_ and _PowerPlans_ in _PowerChart_.
  * @action `launchView` - (optional) Sets the view to be displayed.If not provided,
  * will default to `search` view.
- * @action `enablePowerPlans` - (optional) Enables power plans in the MOEW. Power plans are disabled by default.
- * **NOTE**: Our internal testing suggests there is a _PowerChart_ bug relating to enabling this option
- * where making MPAGES_EVENT calls, through `submitOrders`, in series with this option enabled will lead
- * to some MPAGES_EVENT calls failing to be invoked. Please keep this in mind when enabling this option.
  * @action `silentSign` - (optional) Signs the orders silently. Orders are not signed silently by default.
- *
  * @documentation [MPAGES_EVENT - ORDER](https://wiki.cerner.com/display/public/MPDEVWIKI/MPAGES_EVENT+-+ORDERS)
+ * @warning Our internal testing suggests there is a _PowerChart_ bug relating to the use of power
+ * orders. When making MPAGES_EVENT calls (which we do in this library through `submitOrders`)
+ * in series with power orders enabled, the result was that some MPAGES_EVENT calls failed to be invoked.
+ * Please keep this in mind when enabling this option. For our own use, we have disabled power orders when
+ * using `submitOrders` in such a way that calls are made in series.
  */
 export type SubmitOrderOpts = {
   targetTab?: 'orders' | 'power orders' | 'medications' | 'power medications';
   launchView?: 'search' | 'profile' | 'signature';
-  enablePowerPlans?: boolean;
   signSilently?: boolean;
 };
 
 /**
- * Submit orders for a patient in a given encounter through the Cerner PowerChart MPage Event interface.
- * By default, power plans are disabled (potential bug in PowerChart), the target tab is set to order
- * with power orders enabled, and will launch to the signature view.
+ * Submit orders for a patient in a given encounter through the _Cerner PowerChart_ `MPAGES_EVENT` function.
+ * By default, _PowerPlans_ are disabled (potential bug in _PowerChart_), _PowerOrders_ are disabled,
+ * the target tab is set to orders, and will launch to the signature view.
  * @param {number} personId - The identifier for the patient to whom the note belongs.
  * Cerner context variable: PAT_PersonId.
  * @param {number} encounterId - The identifier for the encounter belonging to the patient where
  * this note will be launched. Cerner context variable: VIS_EncntrId.
- * @param orders - The orders to be submitted. Orders are given in the form of an
- * Cerner MPage Order string of pipe-delimited parameters.
- * @param opts - (optional) User defined options for the order submission event.
+ * @param orders - The orders to be submitted. Orders are given in the form of a a series of pipe-delimited
+ * parameters as specified in the `MPAGES_EVENT` documentation (below). Use the `fluent-cerner-js` library's
+ * `orderString` function to simplify building these pipe-delimited order strings.
+ * @param opts - (optional) User defined options for the order submission event. The options allow for
+ * changing the target tab, the view to be launched, and whether or not the orders should be signed silently.
  * @returns an object with the order `eventString` and a boolean flag set to notify the user if
  * the attempt was made outside of PowerChart, `inPowerChart`.
  *
@@ -54,9 +56,11 @@ export const submitOrders = (
   orders: Array<string>,
   opts?: SubmitOrderOpts
 ): { eventString: string; inPowerChart: boolean } => {
-  let { targetTab, launchView, enablePowerPlans, signSilently } = opts || {};
-  if (!targetTab) targetTab = 'power orders';
+  let { targetTab, launchView, signSilently } = opts || {};
+  if (!targetTab) targetTab = 'orders';
   if (!launchView) launchView = 'signature';
+  const enablePowerPlans =
+    targetTab === 'power orders' || targetTab === 'power medications';
 
   let inPowerChart = true;
 
