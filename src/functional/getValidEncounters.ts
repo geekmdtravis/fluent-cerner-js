699f3d604b4ecb78285e41c9f4e4efdae99622db
geekmdtravis <travis@geekmd.io>
2023-07-26 15:52:40 -0700

    updated JSDoc

--- src/functional/getValidEncounters.ts
+++ src/functional/getValidEncounters.ts
@@ -2,9 +2,11 @@ import { PowerChartReturn } from '.';
 import { outsideOfPowerChartError } from '../utils';
 
 /**
- *
+ * Get a list of valid encounter ID's for a given patient.
  * @param pid {number} - the patient ID to get valid encounters for
- * @returns
+ * @returns a `Promise` which resolves to an object containing an array of valid encounter IDs
+ * and a boolean indicating whether the user is in PowerChart
+ * @throws an error if an unexpected error occurs.
  */
 export async function getValidEncountersAsync(
   pid: number
