ce8b31aec4913123bc302f7b260e932982210a7b
Daniel.Lara2 <daniel.lara2@gcdcr.ca.gov>
2023-08-08 13:52:07 -0700

    create addPowerPlanWithDetailsAsync()

---
+++ src/functional/addPowerPlanWithDetails.ts
@@ -0,0 +1,76 @@
+import { PowerChartReturn } from '.';
+import { outsideOfPowerChartError } from '../utils';
+import { PowerPlanOrder } from './submitPowerPlanOrders';
+
+/**
+ * Attempts to add a PowerPlan and creates PowerPlan objects from the pathway catalog IDs. CreateMOEW() must be called first.
+ * @param moewHandle {number} - the handle to the MOEW.
+ * @param eid {number} - the encounter ID in which orders would be placed
+ * @param powerPlanOrders {Array<PowerPlanOrder>} - An array of pathway catalog IDs for PowerPlan orders to be placed.
+ * @returns a `Promise` which resolves to a boolean, indicating whether or not the PowerPlan orders were successfully added
+ * @throws `Error` if an unexpected error occurs or if the array provided is empty
+ */
+
+export async function addPowerPlanWithDetailsAsync(
+  moewHandle: number,
+  powerPlanOrders: Array<PowerPlanOrder>
+): Promise<
+  PowerChartReturn & {
+    powerPlanAdded: boolean;
+  }
+> {
+
+  //Prepare the default return data
+  let retData: {
+    inPowerChart: boolean;
+    powerPlanAdded: boolean;
+  } = {
+    inPowerChart: true,
+    powerPlanAdded: true,
+  };
+
+  // Check for to make sure the array of orders provided is not empty
+  if (powerPlanOrders.length < 1) {
+    throw new RangeError(
+      'There should be at least one PowerPlan order provided.'
+    );
+  }
+
+  // Convert the PowerPlan orders provided into the required XML string
+  let powerPlanOrdersXML: string = '';
+
+  powerPlanOrders.forEach(powerPlanOrder => {
+    powerPlanOrdersXML += `<Plan><PathwayCatalogId>${powerPlanOrder}</PathwayCatalogId><PersonalizedPlanId>0.0</PersonalizedPlanId><Diagnoses></Diagnoses></Plan>`;
+  });
+
+  // Add <Plans> to beginning & end of PowerPlan XML
+  powerPlanOrdersXML = '<Plans>' + powerPlanOrdersXML;
+  powerPlanOrdersXML += '</Plans>';
+
+  // Create the DiscernObjectFactory and use that to call AddPowerPlanWithDetails() with the values from above
+  try {
+    const dcof = await window.external.DiscernObjectFactory('POWERORDERS');
+    const response = await dcof.AddPowerPlanWithDetails(
+      moewHandle,
+      powerPlanOrdersXML
+    );
+
+    // Set the `powerPlanAdded` boolean to be true if the orders were added, and false otherwise
+    retData.powerPlanAdded = response === 0 ? false : true;
+
+  } catch (e) {
+
+    // If outside of PowerChart, set the output to reflect that
+    if (outsideOfPowerChartError(e)) {
+      retData.inPowerChart = false;
+      retData.powerPlanAdded = false;
+    } 
+    else {
+      // If some other error was encountered, throw that error
+      throw e;
+    }
+  }
+
+  // Return the retData object when complete
+  return retData;
+}
--- src/index.ts
+++ src/index.ts
@@ -141,6 +141,18 @@ declare global {
       ) => Promise<number>;
 
       /**
+       * Creates PowerPlan objects from the pathway catalog IDs. CreateMOEW() must be called first.
+       * @param lMOEWHandle {number} - the handle to the MOEW
+       * @param planDetailsXMLBstr {string} - XML string containing the plan/pathway catalog IDs
+       * @returns a `Promise` which resolves to an integer: 1 if the plan was addedd successfully, and 0 otherwise.
+       * @throws `Error` if an unexpected error occurs
+       */
+      AddPowerPlanWithDetails: (
+        lMOEWHandle: number,
+        planDetailsXMLBstr: string
+      ) => Promise<number>;
+
+      /**
        * Get valid encounter ID's for a given patient.
        * @param pid {number} - the patient ID of the patient to get encounters for.
        * @returns a `Promise` of a string representing the valid encounter ID's for the patient.
