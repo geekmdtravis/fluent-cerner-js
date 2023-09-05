1aaed530ae913228ac861579a8b26e0d5613bbbb
geekmdtravis <travis@geekmd.io>
2023-09-05 09:24:40 -0700

    simplified implmenetation of openWebsiteByUrlAsync

--- src/functional/openWebsiteByUrlAsync.ts
+++ src/functional/openWebsiteByUrlAsync.ts
@@ -1,27 +1,14 @@
-import { PowerChartReturn } from '.';
-import { outsideOfPowerChartError } from '../utils';
+import { ApplinkReturn } from '.';
+import { openApplicationAsync } from './openApplicationAsync';
 
 export async function openWebsiteByUrlAsync(
   url: string
-): Promise<PowerChartReturn & { success: boolean }> {
+): Promise<ApplinkReturn> {
   if (!url.includes('http://') && !url.includes('https://')) {
     throw new Error(
       "openWebsiteByUrlAsync: URL must include 'http://' or 'https://'"
     );
   }
-  const retVal: PowerChartReturn & { success: boolean } = {
-    inPowerChart: true,
-    success: false,
-  };
-  try {
-    const response = await window.external.APPLINK(100, url, '');
-    retVal.success = response === null ? true : false;
-  } catch (e) {
-    if (outsideOfPowerChartError(e)) {
-      retVal.inPowerChart = false;
-    } else {
-      throw e;
-    }
-  }
-  return retVal;
+
+  return await openApplicationAsync('by url', url);
 }
