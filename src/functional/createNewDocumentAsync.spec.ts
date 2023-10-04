0455eeba42060cdb4b044ddaaa9928da1219d960
geekmdtravis <travis@geekmd.io>
2023-10-04 14:14:34 -0700

    enhanced unit tests for createNewDocumentAsync

--- src/functional/createNewDocumentAsync.spec.ts
+++ src/functional/createNewDocumentAsync.spec.ts
@@ -36,153 +36,127 @@ describe('createNewDocumentAsync', () => {
     );
     expect(inPowerChart).toBe(true);
   });
-  it('returns inPowerChart as true when inside of PowerChart when created by reference ID with note code given', async () => {
+  it("returns 'success' as 'true' when 'by workflow' method is used and a successful action is returned", async () => {
+    Object.defineProperty(window, 'external', {
+      writable: true,
+      value: {
+        DiscernObjectFactory: jest.fn().mockImplementation(() => ({
+          OpenDynDocByWorkFlowId: async () => Promise.resolve(1),
+        })),
+      },
+    });
+    const { success } = await createNewDocumentAsync('by workflow', 1, 1, 1);
+    expect(success).toBe(true);
+  });
+  it("returns 'success' as 'false' when 'by workflow' method is used and a unsuccessful action is returned", async () => {
+    Object.defineProperty(window, 'external', {
+      writable: true,
+      value: {
+        DiscernObjectFactory: jest.fn().mockImplementation(() => ({
+          OpenDynDocByWorkFlowId: async () => Promise.resolve(0),
+        })),
+      },
+    });
+    const { success } = await createNewDocumentAsync('by workflow', 1, 1, 1);
+    expect(success).toBe(false);
+  });
+  it("returns 'success' as 'true' when 'by reference template' method without a 'noteTypeCd' is used and a successful action is returned", async () => {
+    Object.defineProperty(window, 'external', {
+      writable: true,
+      value: {
+        DiscernObjectFactory: jest.fn().mockImplementation(() => ({
+          OpenNewDocumentByReferenceTemplateId: async () => Promise.resolve(1),
+        })),
+      },
+    });
+    const { success } = await createNewDocumentAsync(
+      'by reference template',
+      1,
+      1,
+      1
+    );
+    expect(success).toBe(true);
+  });
+  it("returns 'success' as 'true' when 'by reference template' method without a 'noteTypeCd' is used and a unsuccessful action is returned", async () => {
+    Object.defineProperty(window, 'external', {
+      writable: true,
+      value: {
+        DiscernObjectFactory: jest.fn().mockImplementation(() => ({
+          OpenNewDocumentByReferenceTemplateId: async () => Promise.resolve(0),
+        })),
+      },
+    });
+    const { success } = await createNewDocumentAsync(
+      'by reference template',
+      1,
+      1,
+      1
+    );
+    expect(success).toBe(false);
+  });
+  it("returns 'success' as 'true' when 'by reference template' method with a 'noteTypeCd' is used and a successful action is returned", async () => {
     Object.defineProperty(window, 'external', {
       writable: true,
       value: {
         DiscernObjectFactory: jest.fn().mockImplementation(() => ({
           OpenNewDocumentByReferenceTemplateIdAndNoteType: async () =>
-            Promise.resolve(null),
+            Promise.resolve(1),
         })),
       },
     });
-    const { inPowerChart } = await createNewDocumentAsync(
+    const { success } = await createNewDocumentAsync(
       'by reference template',
       1,
       1,
       1,
       1
     );
-    expect(inPowerChart).toBe(true);
+    expect(success).toBe(true);
   });
-  it('returns inPowerChart as false when outside of PowerChart when created by workflow ID', async () => {
-    const { inPowerChart } = await createNewDocumentAsync(
-      'by workflow',
-      1,
-      1,
-      1
-    );
-    expect(inPowerChart).toBe(false);
-  });
-  it('returns inPowerChart as true when inside of PowerChart when created by workflow ID', async () => {
+  it("returns 'success' as 'true' when 'by reference template' method with a 'noteTypeCd' is used and a successful action is returned", async () => {
     Object.defineProperty(window, 'external', {
       writable: true,
       value: {
         DiscernObjectFactory: jest.fn().mockImplementation(() => ({
-          OpenDynDocByWorkFlowId: async () => Promise.resolve(null),
+          OpenNewDocumentByReferenceTemplateIdAndNoteType: async () =>
+            Promise.resolve(0),
         })),
       },
     });
-    const { inPowerChart } = await createNewDocumentAsync(
-      'by workflow',
+    const { success } = await createNewDocumentAsync(
+      'by reference template',
+      1,
       1,
       1,
       1
     );
-    expect(inPowerChart).toBe(true);
+    expect(success).toBe(false);
   });
-  it('throws an Error when there is neither a refTemplate ID nor a workflow ID provide', async () => {
-    try {
-      await createNewDocumentAsync('by workflow', 1, 1, 1);
-    } catch (e) {
-      expect(e).toBeInstanceOf(Error);
-      expect(e as Error).toHaveProperty(
-        'message',
-        'Either a reference template ID or a workflow ID must be provided.'
-      );
-    }
+  it('throws Error when an unsupported method is used', async () => {
+    Object.defineProperty(window, 'external', {
+      writable: true,
+      value: {
+        DiscernObjectFactory: jest.fn().mockImplementation(() => ({})),
+      },
+    });
+    await expect(
+      // @ts-ignore
+      createNewDocumentAsync('unsupported', 1, 1, 1)
+    ).rejects.toThrow(
+      'unsupported is not supported as a method of creating a new document.'
+    );
   });
-  it('throws an Error when there is both a refTemplate ID and a workflow ID provided', async () => {
-    try {
-      await createNewDocumentAsync('by reference template', 1, 1, 1, 1);
-    } catch (e) {
-      expect(e).toBeInstanceOf(Error);
-      expect(e as Error).toHaveProperty(
-        'message',
-        'Only one of a reference template ID or a workflow ID can be provided, exclusively.'
-      );
-    }
-  });
-  it('throws an Error when there is no workflow ID provided when creating a document by workflow', async () => {
-    try {
-      await createNewDocumentAsync('by workflow', 1, 1, 1);
-    } catch (e) {
-      expect(e).toBeInstanceOf(Error);
-      expect(e as Error).toHaveProperty(
-        'message',
-        'A workflow ID must be provided when creating a document by workflow.'
-      );
-    }
-  });
-  it('throws an Error when there is no refTemplate ID provided when creating a document by reference template', async () => {
-    try {
-      await createNewDocumentAsync('by reference template', 1, 1, 1);
-    } catch (e) {
-      expect(e).toBeInstanceOf(Error);
-      expect(e as Error).toHaveProperty(
-        'message',
-        'A reference template ID must be provided when creating a document by reference template.'
-      );
-    }
-  });
-  it('calls console.warn when a note type code is provided when creating a document by workflow', async () => {
-    const spy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+  it('console.warn is called when a noteTypeCd is provided when using the by workflow method', async () => {
+    Object.defineProperty(window, 'external', {
+      writable: true,
+      value: {
+        DiscernObjectFactory: jest.fn().mockImplementation(() => ({
+          OpenDynDocByWorkFlowId: async () => Promise.resolve(0),
+        })),
+      },
+    });
+    const consoleSpy = jest.spyOn(console, 'warn');
     await createNewDocumentAsync('by workflow', 1, 1, 1, 1);
-    expect(spy).toHaveBeenCalled();
-    spy.mockRestore();
-  });
-  it('returns success as true when the DynDoc window is successfully called', async () => {
-    // TODO: implement after investing if return values are as expected for the underlying COM object methods
-    expect(true).toBe(false);
-  });
-  it('returns success as false when the DynDoc window is not successfully called', async () => {
-    // TODO: implement after investing if return values are as expected for the underlying COM object methods
-    expect(true).toBe(false);
-  });
-  it('throws an Error when an unexpected error occurs when attempting to create new document by workflow id', async () => {
-    Object.defineProperty(window, 'external', {
-      writable: true,
-      value: {
-        DiscernObjectFactory: jest.fn().mockImplementation(() => ({
-          OpenDynDocByWorkFlowId: async () => {
-            throw new Error('test');
-          },
-        })),
-      },
-    });
-    return expect(
-      async () => await createNewDocumentAsync('by workflow', 1, 1, 1)
-    ).rejects.toThrow(Error);
-  });
-  it('throws an Error when an unexpected error occurs when attempting to create new document by referene template', async () => {
-    Object.defineProperty(window, 'external', {
-      writable: true,
-      value: {
-        DiscernObjectFactory: jest.fn().mockImplementation(() => ({
-          OpenDynDocByWorkFlowId: async () => {
-            throw new Error('test');
-          },
-        })),
-      },
-    });
-    return expect(
-      async () => await createNewDocumentAsync('by reference template', 1, 1, 1)
-    ).rejects.toThrow(Error);
-  });
-  it('successfully create a new document by reference template when a note type code is provided', async () => {
-    // TODO: implement after investing if return values are as expected for the underlying COM object methods
-    expect(true).toBe(false);
-  });
-  it('successfully create a new document by reference template when a note type code is not provided', async () => {
-    // TODO: implement after investing if return values are as expected for the underlying COM object methods
-    expect(true).toBe(false);
-  });
-  it('returns success as true when the DynDoc window is successfully called when creating a document by reference template', async () => {
-    // TODO: implement after investing if return values are as expected for the underlying COM object methods
-    expect(true).toBe(false);
-  });
-  it('returns success as false when the DynDoc window is not successfully called when creating a document by workflow id', async () => {
-    // TODO: implement after investing if return values are as expected for the underlying COM object methods
-    expect(true).toBe(false);
+    expect(consoleSpy).toHaveBeenCalled();
   });
 });
--- src/functional/createNewDocumentAsync.ts
+++ src/functional/createNewDocumentAsync.ts
@@ -13,11 +13,7 @@ import { outsideOfPowerChartError } from '../utils';
  * @resolves {PowerChartReturn & { success: boolean }} - a `Promise` which resolves to an object
  * containing a boolean indicating whether the user is in PowerChart and a boolean indicating
  * whether the action was successful.
- * @throws {Error} if an unexpected error occurs.
- * @throws {Error} if neither a reference template ID nor a workflow ID is provided.
- * @throws {Error} if both a reference template ID and a workflow ID are provided.
- * @throws {Error} if a workflow ID is not provided when creating a document by workflow.
- * @throws {Error} if a reference template ID is not provided when creating a document by reference template.
+ * @throws {Error} - an error is thrown if the method provided as an argument is not supported.
  */
 export async function createNewDocumentAsync(
   method: 'by workflow' | 'by reference template',
@@ -26,68 +22,57 @@ export async function createNewDocumentAsync(
   id: number,
   noteTypeCd?: number
 ): Promise<PowerChartReturn & { success: boolean }> {
-  if (method === 'by workflow' && noteTypeCd) {
-    console.warn(
-      'A note type code was provided, but it will be ignored when creating a document by workflow'
-    );
-  }
-
   let retVal: PowerChartReturn & { success: boolean } = {
     inPowerChart: true,
     success: false,
   };
 
-  if (method === 'by workflow') {
-    try {
-      const dcof = await window.external.DiscernObjectFactory('DYNDOC');
-      let response: 0 | 1 | null = null;
+  try {
+    const dcof = await window.external.DiscernObjectFactory('DYNDOC');
+
+    let response: 0 | 1 | null = null;
+
+    if (method !== 'by workflow' && method !== 'by reference template') {
+      throw new Error(
+        `${method} is not supported as a method of creating a new document.`
+      );
+    }
+
+    if (method === 'by workflow' && noteTypeCd) {
+      console.warn(
+        'The noteTypeCd is not used when creating a document by workflow.'
+      );
+    }
+
+    if (method === 'by workflow') {
       response = await dcof.OpenDynDocByWorkFlowId(personId, encounterId, id);
-      if (response === 1) {
-        retVal.success = true;
-      }
-      return retVal;
-    } catch (e) {
-      if (outsideOfPowerChartError(e)) {
-        retVal.inPowerChart = false;
-        return retVal;
-      } else {
-        throw e;
-      }
+    }
+
+    if (method === 'by reference template' && !noteTypeCd) {
+      response = await dcof.OpenNewDocumentByReferenceTemplateId(
+        personId,
+        encounterId,
+        id
+      );
+    }
+
+    if (method === 'by reference template' && noteTypeCd) {
+      response = await dcof.OpenNewDocumentByReferenceTemplateIdAndNoteType(
+        personId,
+        encounterId,
+        id,
+        noteTypeCd
+      );
+    }
+
+    retVal.success = Boolean(response);
+  } catch (e) {
+    if (outsideOfPowerChartError(e)) {
+      retVal.inPowerChart = false;
+    } else {
+      throw e;
     }
   }
 
-  if (method === 'by reference template') {
-    try {
-      const dcof = await window.external.DiscernObjectFactory('DYNDOC');
-      let response: 0 | 1 | null = null;
-      if (noteTypeCd) {
-        response = await dcof.OpenNewDocumentByReferenceTemplateIdAndNoteType(
-          personId,
-          encounterId,
-          id,
-          noteTypeCd
-        );
-      } else {
-        response = await dcof.OpenNewDocumentByReferenceTemplateId(
-          personId,
-          encounterId,
-          id
-        );
-        if (response === 1) {
-          retVal.success = true;
-        }
-      }
-      return retVal;
-    } catch (e) {
-      if (outsideOfPowerChartError(e)) {
-        retVal.inPowerChart = false;
-        return retVal;
-      } else {
-        throw e;
-      }
-    }
-  }
-  throw new Error(
-    `An unexpected error occurred when attempting to create a new document by ${method}.`
-  );
+  return retVal;
 }
